eaf206a1ac480156b335b512422d183d959b8f8d
Nate Buckareff <nate.buckareff@gmail.com>
2026-04-06 10:42:22 -0700

    added `BufferView` and tightened `Map` safety

---
+++ crates/renderer/src/gpu/buffer_view.rs
@@ -0,0 +1,103 @@
+use std::marker::PhantomData;
+
+use anyhow::{Result, anyhow};
+use bytemuck::Pod;
+
+use crate::gpu::Map;
+
+pub struct BufferView<T: Pod> {
+    map: Map,
+    len: u64,
+    marker: PhantomData<T>,
+}
+
+impl<T: Pod> BufferView<T> {
+    pub fn new(map: Map) -> Result<Self> {
+        let stride = std::mem::size_of::<T>() as u64;
+        if stride == 0 {
+            return Err(anyhow!("zero-sized types are invalid"));
+        }
+
+        let alignment = std::mem::align_of::<T>() as u64;
+        if map.alignment() < alignment {
+            return Err(anyhow!("map and view are unaligned"));
+        }
+
+        let byte_len = map.len();
+        if byte_len % stride != 0 {
+            return Err(anyhow!("map size is not a multiple of stride"));
+        }
+
+        Ok(Self {
+            map,
+            len: byte_len / stride,
+            marker: PhantomData,
+        })
+    }
+
+    pub fn len(&self) -> u64 {
+        self.len
+    }
+
+    fn translate_range(&self, range: std::ops::Range<u64>) -> std::ops::Range<u64> {
+        let size = std::mem::size_of::<T>() as u64;
+        // OVERFLOW: callers only pass ranges with `end <= self.len`. In
+        // `new()`, `self.len` is computed as `map.len() / size`, so `self.len *
+        // size <= map.len()`.  Since `map.len()` is a `u64`, both `range.start
+        // * size` and `range.end * size` are bounded by `self.len * size` and
+        // therefore cannot overflow.
+        let start = range.start * size;
+        let end = range.end * size;
+        start..end
+    }
+}
+
+impl<T: Pod> std::ops::Index<u64> for BufferView<T> {
+    type Output = T;
+
+    fn index(&self, index: u64) -> &Self::Output {
+        // TODO: doing bounds check twice
+        assert!(index < self.len, "buffer view index out-of-bounds");
+
+        let range = self.translate_range(index..index + 1);
+        // SAFETY: assert guards this
+        bytemuck::from_bytes(unsafe { self.map.get_range_unchecked(range) })
+    }
+}
+
+impl<T: Pod> std::ops::IndexMut<u64> for BufferView<T> {
+    fn index_mut(&mut self, index: u64) -> &mut Self::Output {
+        // TODO: doing bounds check twice
+        assert!(index < self.len, "buffer view index out-of-bounds");
+
+        let range = self.translate_range(index..index + 1);
+        // SAFETY: assert guards this
+        bytemuck::from_bytes_mut(unsafe { self.map.get_range_unchecked_mut(range) })
+    }
+}
+
+impl<T: Pod> std::ops::Index<std::ops::Range<u64>> for BufferView<T> {
+    type Output = [T];
+
+    fn index(&self, range: std::ops::Range<u64>) -> &Self::Output {
+        assert!(range.start <= self.len(), "buffer view out-of-bounds");
+        assert!(range.end <= self.len(), "buffer view out-of-bounds");
+        assert!(range.start <= range.end, "invalid buffer view range");
+
+        let range = self.translate_range(range);
+        // SAFETY: assert guards this
+        bytemuck::cast_slice(unsafe { self.map.get_range_unchecked(range) })
+    }
+}
+
+impl<T: Pod> std::ops::IndexMut<std::ops::Range<u64>> for BufferView<T> {
+    fn index_mut(&mut self, range: std::ops::Range<u64>) -> &mut Self::Output {
+        assert!(range.start <= self.len(), "buffer view out-of-bounds");
+        assert!(range.end <= self.len(), "buffer view out-of-bounds");
+        assert!(range.start <= range.end, "invalid buffer view range");
+
+        let range = self.translate_range(range);
+        // SAFETY: assert guards this
+        bytemuck::cast_slice_mut(unsafe { self.map.get_range_unchecked_mut(range) })
+    }
+}
--- crates/renderer/src/gpu/map.rs
+++ crates/renderer/src/gpu/map.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 
 use crate::gpu::{Buffer, BufferObject, BufferSpan, BufferWriter, Range};
 
+// TODO: rename to BufferMap
 pub struct Map {
     // SAFETY: buffer first so it is destructed before the Arc<Buffer> in span
     // is dropped
@@ -21,7 +22,7 @@ impl Map {
         // long as each other
         let buffer = unsafe { std::mem::transmute(buf.as_ref()) };
         let mapping = BufferMapping::new(buffer)?;
-        let base = mapping.translate(span.range().start());
+        let base = mapping.translate_offset(span.range().start());
         Ok(Self {
             buffer,
             mapping,
@@ -37,6 +38,11 @@ impl Map {
 
     pub fn span(&self) -> &BufferSpan {
         &self.span
+    }
+
+    // pre-translated base offset
+    pub fn base(&self) -> u64 {
+        self.base
     }
 
     pub fn mapping(&self) -> &BufferMapping {
@@ -66,6 +72,37 @@ impl Map {
         Ok(range)
     }
 
+    // this and get_range_unchecked_mut are factored out so BufferView can does
+    // not double-assert
+    pub(crate) unsafe fn get_range_unchecked(&self, range: std::ops::Range<u64>) -> &[u8] {
+        // OVERFLOW: will not overflow when `range.start <= range.end`
+        let size: u64 = range.end - range.start;
+
+        // OVERFLOW: if `range.start` is in range, then this will not overflow,
+        // as the span was already constructed relative to the base offset
+        let ptr = (self.base + range.start) as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, size as usize) }
+    }
+
+    pub(crate) unsafe fn get_range_unchecked_mut(
+        &mut self,
+        range: std::ops::Range<u64>,
+    ) -> &mut [u8] {
+        let span_start = self.span.range().start();
+        // OVERFLOW: will not overflow for the same reasoning as below
+        let start = span_start + range.start;
+        let end = span_start + range.end;
+        self.dirty.borrow_mut().mark(Range::new(start, end));
+
+        // OVERFLOW: will not overflow when `range.start <= range.end`
+        let size = range.end - range.start;
+
+        // OVERFLOW: if `range.start` is in range, then this will not overflow,
+        // as the span was already constructed relative to the base offset
+        let ptr = (self.base + range.start) as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, size as usize) }
+    }
+
     pub fn writer(self) -> Result<BufferWriter> {
         BufferWriter::new(self)
     }
@@ -83,6 +120,7 @@ impl Map {
     }
 }
 
+// TODO: rename to Mapping
 pub struct BufferMapping {
     pointer: NonNull<u8>,
     size: u64,
@@ -103,9 +141,16 @@ impl BufferMapping {
         self.size
     }
 
-    pub fn translate(&self, offset: u64) -> u64 {
+    pub fn translate_offset(&self, offset: u64) -> u64 {
         assert!(offset <= self.size);
         self.pointer.as_ptr() as u64 + offset
+    }
+
+    pub fn translate_range(&self, range: Range) -> Range {
+        assert!(range.end() <= self.size);
+        let start = self.translate_offset(range.start());
+        let end = self.translate_offset(range.end());
+        Range::new(start, end)
     }
 }
 
@@ -148,7 +193,7 @@ impl std::ops::Index<u64> for Map {
     type Output = u8;
 
     fn index(&self, index: u64) -> &Self::Output {
-        assert!(index < self.len(), "map index out-of-range");
+        assert!(index < self.len(), "map index out-of-bounds");
         unsafe {
             // OVERFLOW: if `index` is in range, then this will not overflow, as
             // the span was already constructed relative to the base offset
@@ -160,7 +205,7 @@ impl std::ops::Index<u64> for Map {
 
 impl std::ops::IndexMut<u64> for Map {
     fn index_mut(&mut self, index: u64) -> &mut Self::Output {
-        assert!(index < self.len(), "map index out-of-range");
+        assert!(index < self.len(), "map index out-of-bounds");
         unsafe {
             let start = self.span.range().start() + index;
             self.dirty.borrow_mut().mark(Range::new(start, start + 1));
@@ -177,38 +222,22 @@ impl std::ops::Index<std::ops::Range<u64>> for Map {
     type Output = [u8];
 
     fn index(&self, range: std::ops::Range<u64>) -> &Self::Output {
-        assert!(range.start <= self.len(), "map index out-of-range");
-        assert!(range.end <= self.len(), "map index out-of-range");
+        assert!(range.start <= self.len(), "map index out-of-bounds");
+        assert!(range.end <= self.len(), "map index out-of-bounds");
         assert!(range.start <= range.end, "invalid map range");
 
-        let size = range.end - range.start;
-        assert!(size <= usize::MAX as u64, "map slice too large");
-
-        // OVERFLOW: if `range.start` is in range, then this will not overflow,
-        // as the span was already constructed relative to the base offset
-        let ptr = (self.base + range.start) as *const u8;
-        unsafe { std::slice::from_raw_parts(ptr, size as usize) }
+        // SAFETY: asserts guard this
+        unsafe { self.get_range_unchecked(range) }
     }
 }
 
 impl std::ops::IndexMut<std::ops::Range<u64>> for Map {
     fn index_mut(&mut self, range: std::ops::Range<u64>) -> &mut Self::Output {
-        assert!(range.start <= self.len(), "map index out-of-range");
-        assert!(range.end <= self.len(), "map index out-of-range");
+        assert!(range.start <= self.len(), "map index out-of-bounds");
+        assert!(range.end <= self.len(), "map index out-of-bounds");
         assert!(range.start <= range.end, "invalid map range");
 
-        let span_start = self.span.range().start();
-        // OVERFLOW: will not overflow for the same reasoning as below
-        let start = span_start + range.start;
-        let end = span_start + range.end;
-        self.dirty.borrow_mut().mark(Range::new(start, end));
-
-        let size = range.end - range.start;
-        assert!(size <= usize::MAX as u64, "map slice too large");
-
-        // OVERFLOW: if `range.start` is in range, then this will not overflow,
-        // as the span was already constructed relative to the base offset
-        let ptr = (self.base + range.start) as *mut u8;
-        unsafe { std::slice::from_raw_parts_mut(ptr, size as usize) }
+        // SAFETY: asserts guard this
+        unsafe { self.get_range_unchecked_mut(range) }
     }
 }
